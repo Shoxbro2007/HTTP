5965aa1c422e30583235d2d59bbfbe6723b257c4
Shoxbro2007 <Shoxbro2007@gmail.com>
2025-07-02 18:03:00 +0300

    Добавлен блок Портфолио

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ function toggleMenu() {
   nav.classList.toggle("active");
 }
 
+const toggle = document.getElementById("theme-toggle");
+const body = document.body;
+
+if (localStorage.getItem("theme") === "dark") {
+  body.classList.add("dark-mode");
+  toggle.textContent = "🌞";
+}
+
+toggle.addEventListener("click", () => {
+  body.classList.toggle("dark-mode");
+  const isDark = body.classList.contains("dark-mode");
+  toggle.textContent = isDark ? "🌞" : "🌙";
+  localStorage.setItem("theme", isDark ? "dark" : "light");
+});
+
+
